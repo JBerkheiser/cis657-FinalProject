cf7a44d399d82c1038cee6e38bc9d8b7eee27aa8
Joe Berkheiser <berkheij@mail.gvsu.edu>
2024-06-15 13:45:14 -0400

    Added commented out analytic code

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SortProvider } from './components/SortContext';
 import { AlbumProvider } from './components/AlbumContext';
 import LoginStack from './components/LoginStack';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import initAlbumDB from './helpers/fb-albums';
+// import analytics from "@react-native-firebase/analytics";
 
 export default function App() 
 {
+  // const navigationRef = useRef();
+  // const routeNameRef = useRef();
+
   useEffect(() =>
   {
       try
@@ -24,7 +28,24 @@ export default function App()
     <SafeAreaView style={styles.container}>
     <AlbumProvider>
     <SortProvider>
-    <NavigationContainer>
+    <NavigationContainer
+      // ref={navigationRef}
+      // onReady={() =>
+      //   (routeNameRef.current = navigationRef.current.getCurrentRoute().name)
+      // }
+      // onStateChange={ async () => {
+      //   const previousRouteName = routeNameRef.current;
+      //   const currentRouteName = navigationRef.current.getCurrentRoute().name;
+      //   if (previousRouteName !== currentRouteName) {
+      //     await analytics().logEvent("screen_view", {
+      //       screen_name: currentRouteName,
+      //       screen_class: currentRouteName,
+      //     });
+      //   }
+      //   // Save the current route name for later comparison
+      //   routeNameRef.current = currentRouteName;
+      // }}
+    >
       <LoginStack/>
     </NavigationContainer>
     </SortProvider>
